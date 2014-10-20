f415bc2d804e2aad99106caec635d1f60e1102b5
kapehh <git@kapehh.net>
2014-10-20 20:13:53 +0400

    event join

--- src/me/kapehh/PlayerRespawnM/PlayerRespawnMListener.java
+++ src/me/kapehh/PlayerRespawnM/PlayerRespawnMListener.java
@@ -4,6 +4,7 @@ import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
@@ -20,8 +21,16 @@ public class PlayerRespawnMListener implements Listener {
         }
     }
 
-    @EventHandler(priority = EventPriority.LOWEST)
+    /*@EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerQuit(PlayerQuitEvent event) {
+        Location location = PlayerRespawnM.getRespawnMCollection().process(event.getPlayer());
+        if (location != null) {
+            event.getPlayer().teleport(location);
+        }
+    }*/
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerJoin(PlayerJoinEvent event) {
         Location location = PlayerRespawnM.getRespawnMCollection().process(event.getPlayer());
         if (location != null) {
             event.getPlayer().teleport(location);
